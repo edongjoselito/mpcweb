7b057c113dd4cc5b2cb27100dd309c3b22c38be9
edongjoselito <143322244+edongjoselito@users.noreply.github.com>
2025-07-04 10:48:32 +0800

    commit

--- includes/nav.php
+++ includes/nav.php
@@ -22,10 +22,10 @@
                         <li><a class="dropdown-item" href="criminology.php"><i class="bi bi-shield-lock-fill me-2 text-primary"></i>BS in Criminology</a></li>
                         <li><a class="dropdown-item" href="ece.php"><i class="bi bi-emoji-smile-fill me-2 text-warning"></i>Early Childhood Education</a></li>
                         <li><a class="dropdown-item" href="elementary-ed.php"><i class="bi bi-pencil-fill me-2 text-success"></i>Elementary Education</a></li>
-                        <li><a class="dropdown-item" href="tvte.php"><i class="bi bi-tools me-2 text-info"></i>TVTE</a></li>
+                        <li><a class="dropdown-item" href="tvte.php"><i class="bi bi-tools me-2 text-info"></i>BTVTEd</a></li>
                         <li><a class="dropdown-item" href="bsba.php"><i class="bi bi-briefcase-fill me-2 text-danger"></i>BSBA (MM/FM)</a></li>
                         <li><a class="dropdown-item" href="bshm.php"><i class="bi bi-cup-straw me-2 text-secondary"></i>BSHM</a></li>
-                        <li><a class="dropdown-item" href="high-school.php"><i class="bi bi-book-half me-2 text-dark"></i>High School Program</a></li>
+                        <li><a class="dropdown-item" href="high-school.php"><i class="bi bi-book-half me-2 text-dark"></i>High School/Senior High School Program</a></li>
                         <li><a class="dropdown-item" href="tesda.php"><i class="bi bi-award-fill me-2 text-primary"></i>TESDA NC II / III</a></li>
                     </ul>
                 </li>
